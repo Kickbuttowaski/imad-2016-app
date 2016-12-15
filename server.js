013077fc5f21bf94370e88a696d08575ca5aa9c8
Kickbuttowaski <promods96@gmail.com>
2016-12-15 12:10:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,34 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var content={
+title:'Bairava',
+Heading:'Audio launch',
+content:'Varalam va varalam va '
+};
+
+function createtemp(data)
+{
+    var title=data.title;
+    var Heading=data.Heading;
+    var content=data.content;
+var htmltem=
+    `<html>
+   <title>
+   ${title}
+   </title>
+   
+    <body>
+        <h3>${heading}</h3>
+        <div>
+         ${content}
+        
+        </div>
+        </body>
+</html>`;
+return htmltemp;
+}    
+
 var app = express();
 app.use(morgan('combined'));
 
@@ -9,7 +37,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res) {
-res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+res.send(createtemp(content));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
